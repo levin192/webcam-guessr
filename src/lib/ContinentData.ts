90cec35094b95db1df354d7c7bd765c1e2c0e433
Levin Passlick <>
2022-06-24 14:46:38 +0200

    ✨ write continents in local storage

---
+++ src/lib/ContinentData.ts
@@ -0,0 +1,43 @@
+import DataProvider from './DataProvider';
+
+interface continent {
+    id: string,
+    name: string,
+    count?: number
+}
+
+
+export default class ContinentData {
+    public setData = () => {
+        return new Promise((resolve) => {
+            const localStorage = window.localStorage
+            if (!localStorage.getItem('continentData')) {
+                const continentData = new DataProvider('/list?show=continents');
+                continentData.fetchApiContent().then((data: any) => {
+                    const result = data.result
+                    const continentData = result.continents.filter((continent: continent) => continent.name !== 'Antarctica')
+                    continentData.forEach((continent: continent) => delete continent.count)
+                    localStorage.setItem('continentData', JSON.stringify(continentData))
+                    resolve(data)
+                })
+            } else {
+                console.info('🌏 continent data in localStorage')
+            }
+            return
+        })
+    }
+    public  getData = () => {
+        return new Promise((resolve) => {
+            const continentData = window.localStorage.getItem('continentData')
+            if (continentData) {
+                const data = JSON.parse(continentData)
+                resolve(data)
+            } else {
+               this.setData().then(() => this.getData().then(r => {
+                   resolve(r)
+               }))
+            }
+        })
+    }
+}
+
---
+++ src/lib/SelectGameMode.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import Select, {Option} from '@smui/select';
+
+    let gameMode = ['World', 'Continent', 'Country'];
+
+    let value = 'World';
+</script>
+
+<div>
+    <Select bind:value label="Select Game Mode">
+        {#each gameMode as mode}
+            <Option value={mode}>{mode}</Option>
+        {/each}
+    </Select>
+    {#if value === 'Continent' || value === 'Country'}
+
+    {/if}
+</div>
