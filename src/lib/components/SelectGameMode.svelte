f3dee2e2bde824434b8cfca56b3c07c83c3d2be6
Levin Passlick <>
2022-06-27 09:54:38 +0200

    ✨ Working game modes

--- src/lib/components/SelectGameMode.svelte
+++ src/lib/components/SelectGameMode.svelte
@@ -4,9 +4,10 @@
     import {onMount} from "svelte";
     import ContinentData from "$lib/util/data/ContinentData";
     import CountryData from "$lib/util/data/CountryData";
+    import {gameMode} from "../store";
 
-    let gameMode = ['World', 'Continent', 'Country'];
-    let value = 'World';
+    let gameModes = ['World', 'Continent', 'Country'];
+    let selectedGameMode
     let continents
     let selectedContinent
     let countries
@@ -14,40 +15,61 @@
 
     onMount(() => {
         new ContinentData().getData().then(data => {
-            continents = data.map(item => item.name)
+            continents = data
         })
         new CountryData().getAllCountries().then(data => {
-            countries = data
+            countries = data.sort((a, b) => a.name.localeCompare(b.name))
+            countries = countries.filter(a => {
+               return  a.count >= 40 // Only show countries with more than 40 cams
+            })
         })
     })
 
+    gameMode.subscribe(value => {
+        selectedGameMode = value.mode;
+    });
+    $: if (selectedCountry) {
+        gameMode.set({mode: selectedGameMode, value: selectedCountry.id})
+    }
+    $: if (selectedContinent) {
+        gameMode.set({mode: selectedGameMode, value: selectedContinent})
+    }
+
 
 </script>
-
 <div>
-    <Select bind:value label="Select Game Mode">
-        {#each gameMode as mode}
-            <Option value={mode}>{mode}</Option>
-        {/each}
-    </Select>
-</div>
-{#if value === 'Continent'}
     <div>
-        <!-- ToDo: use select here -->
-        <Autocomplete
-                options={continents}
-                bind:value={selectedContinent}
-                label="Select Continent"
-        />
+        <Select bind:value={selectedGameMode} label="Select Game Mode">
+            {#each gameModes as mode}
+                <Option value={mode}>{mode}</Option>
+            {/each}
+        </Select>
     </div>
-{/if}
-{#if value === 'Country' && countries.length}
-    <Autocomplete
-            options={countries}
-            getOptionLabel={(country) =>
-            country ? `${country.name} (${country.id})` : ''}
-            bind:selectedCountry
-            label="Select Country"
-    />
+    {#if selectedGameMode === 'Continent'}
+        <div>
+            <Select bind:value={selectedContinent} label="Select Continent">
+                {#each continents as continent}
+                    <Option value={continent.id}>{continent.name}</Option>
+                {/each}
+            </Select>
 
-{/if}
+        </div>
+    {/if}
+    {#if selectedGameMode === 'Country' && countries.length}
+        <div>
+            <Autocomplete
+                    options={countries}
+                    getOptionLabel={(country) =>
+            country ? `${country.name}` : ''}
+                    bind:value={selectedCountry}
+                    label="Select Country"
+            />
+        </div>
+    {/if}
+</div>
+
+<style lang="scss">
+  div {
+    margin: 1rem 0
+  }
+</style>
--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,3 +1,7 @@
 import { writable } from 'svelte/store';
 
 export const userPrefDarkMode = writable(false);
+export const gameMode = writable({
+    mode: 'World',
+    value: null
+})
